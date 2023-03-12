0ff47e00ef1e19393baa4795eb87829a51da6d38
Cristian David <cristiandaberu@gmail.com>
2023-03-12 17:41:55 -0500

    FINAL
    Login, gestion de equipos (reserva y soportes)

--- models.py
+++ models.py
@@ -1,5 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Equipos(models.Model):
+    IdEquipo = models.AutoField(primary_key=True)
+    IdUsuario = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    Tipo = models.CharField(max_length=30, null=False)
+    Modelo = models.CharField(max_length=30, null=False)
+    Marca = models.CharField(max_length=30, null=False)
 
+    def getIdEquipo(self):
+        return self.IdEquipo
+    
+
+class Reserva(models.Model):
+    IdReserva = models.AutoField(primary_key=True)
+    IdEquipo = models.ForeignKey(Equipos, on_delete=models.CASCADE, null=False)
+    IdUsuario = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    Detalle = models.CharField(max_length=100, null=True)
+    FechaInicio = models.DateTimeField(null=False)
+    FechaFinal = models.DateTimeField(null=True)
+
+    def getIdEquipo(self):
+        return self.IdEquipo
+    
+
+class Soportes(models.Model):
+    IdSoporte = models.AutoField(primary_key=True)
+    IdEquipo = models.ForeignKey(Equipos, on_delete=models.CASCADE, null=False)
+    IdUsuario = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    Detalle = models.CharField(max_length=100, null=True)
+    FechaInicio = models.DateTimeField(null=False)
+    FechaFinal = models.DateTimeField(null=True)
--- views.py
+++ views.py
@@ -1,7 +1,109 @@
-from django.shortcuts import render
+from datetime import datetime
+from django.db import IntegrityError
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from inventarioucn.models import Equipos, Reserva, Soportes
 
 def inicio(request):
-    return render(request, 'inicio.html')
+    #Reservas y equipos de las reservas
+    try:
+        reservas = Reserva.objects.get(IdUsuario = request.user)
+        equipos = reservas.IdEquipo
+    except:
+        reservas = ()
+        equipos = ()
+    
+    #Soportes y equipos de los soportes
+    try:
+        soportes = Soportes.objects.get(IdUsuario = request.user)
+        equiposSoporte = soportes.IdEquipo
+    except:
+        soportes = ()
+        equiposSoporte = ()
 
-def autenticacion(request):
-    return render(request, 'autenticacion.html')
+    return render(request, 'inicio.html', {
+        'equipos': equipos,
+        'reservas': reservas,
+        'soportes': soportes,
+        'equipoSoporte': equiposSoporte
+    })
+
+def signup(request):
+    if request.method == 'GET':
+        return render(request, 'signup.html', {'form': UserCreationForm})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(
+                    username = request.POST['username'],
+                    password = request.POST['password1'],
+                    first_name = request.POST['firstName'],
+                    last_name = request.POST['lastName'],
+                    email = request.POST['email']
+                )
+                user.save()
+                login(request, user)
+                return redirect('gestion')
+            except IntegrityError:
+                return render(request, 'signup.html', {
+                    'form': UserCreationForm,
+                    'error': 'Usuario ya existe'
+                    })
+
+def gestion(request):
+    if request.method == 'GET':
+        equipos = Equipos.objects.all()
+        return render(request, 'gestion.html', {'equipos': equipos})
+    else:
+        Equipo = request.POST['IdEquipo'] #Revisar si esta linea sirve
+        equipo = Equipos.objects.get(IdEquipo = Equipo)
+        date = datetime.now()
+        reserva = Reserva (
+            IdEquipo = equipo,
+            IdUsuario = request.user,
+            Detalle = request.POST['Detalle'],
+            FechaInicio = date.strftime("%Y-%d-%m %H:%M:%S")
+        )
+        reserva.save()
+        return redirect('inicio')
+
+def signout(request):
+    logout(request)
+    return redirect('signin')
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html')
+    else:
+        user = authenticate(
+            request,
+            username = request.POST['username'],
+            password = request.POST['password']
+        )
+
+        if user is None:
+            return render(request, 'signin.html', {
+                'error': 'Usuario o contraseña incorrecto'
+            })
+        else:
+            login(request, user)
+            return redirect('gestion')
+
+def soporte(request):
+    if request.method == 'GET':
+        return render(request, 'soporte.html')
+    else:
+        Equipo = request.POST['IdEquipo']
+        equipo = Equipos.objects.get(IdEquipo = Equipo)
+        date = datetime.now()
+        soporte = Soportes(
+            FechaInicio = date.strftime("%Y-%d-%m %H:%M:%S"),
+            IdEquipo = equipo,
+            IdUsuario = request.user,
+            Detalle = request.POST['Detalle']
+        )
+        soporte.save()
+        return redirect('inicio')
